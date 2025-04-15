5681350ab0d014a481da110946b01e35ac0aea11
gschool2025 <g.gibson.482@studms.ug.edu.pl>
2025-04-15 15:40:12 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,11 +1,12 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+  String[] names = {"Piotr", "Anna", "Jan", "Leszek"};
 
-        for (int i = 1; i <= 15; i++) {
-        
-            System.out.println("i = " + i);
+        for (int i = 0; i < names.length; i++)
+{
+System.out.println(i + 1 + ". " + names[i]);
+}
         }
     }
-}
+
